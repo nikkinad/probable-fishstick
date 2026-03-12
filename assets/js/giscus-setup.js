40f344129d032fe5d92f15cfa3438b53095a8bcc
nikkinad <156600327+nikkinad@users.noreply.github.com>
2026-03-12 14:03:05 +0000

    Deploying to gh-pages from @ nikkinad/probable-fishstick@c7f6b6b26b6198be0ea45a363a2cb6af6a4fc5f6 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "nikkinad/probable-fishstick",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
